7f47333dc522d5852aa6db2ae562be80ced8e509
Gabriel Silva <102487234+GabrielCarvalhouo@users.noreply.github.com>
2023-06-14 09:10:42 -0300

    Atualização na conexão com o buzzer monitor.ino
    Alterada a ativação do buzzer para sempre desligar antes de ler a temperatura e ligar novamente após o processo.

--- monitor.ino
+++ monitor.ino
@@ -2,7 +2,9 @@
 #include "EspMQTTClient.h"
 
 //variáveis para Json
-char json_btn[100];
+char json_temp[100];
+char json_tensao[100];
+char json_vent[100];
 
 //pinos de entrada e saída
 const int temperatura = 34;
@@ -12,21 +14,22 @@ const int buzzer = 26;
 const int LedRed = 32;
 const int LedYellow = 25;
 
-//variáveis internas ---------------------------------
-//Definindo os valores iniciais
+  //variáveis internas ---------------------------------
+  //Definindo os valores iniciais
 
-float temp = 0;
-int ADCLido = 0;
+  float temp = 0;
+  int ADCLido = 0;
 
-float tensao = 0;
-int tensao_Lida = 0;
+  float tensao = 0;
+  int tensao_Lida = 0;
 
-int inc_buzz = 0;
-bool buzz = false;
+  int inc_buzz = 0;
+  bool buzz = false;
+  int vent = 0;
 
-// ---------------------------------------------------
+  // ---------------------------------------------------
 
-// Realizando a conexão com o servidor
+  // Realizando a conexão com o servidor
 EspMQTTClient client
 (
   "FIESC_IOT", //nome da rede Wi-Fi
@@ -50,7 +53,10 @@ void leitura_sinais()
 {
   ADCLido = analogRead(temperatura);
   Serial.print(ADCLido);
+  digitalWrite(buzzer, LOW); //desaciona o buzzer
+  delay(150);
   temp = (float)ADCLido * (3300.0/2048.0)/10.0;
+  
   Serial.print("  Temperatura = "); //mostra valor na tela
   Serial.print(temp);
   Serial.println(" *C");
@@ -65,61 +71,61 @@ void leitura_sinais()
 
 void verifica_sinais()
 {
-// Valida o valor da temperatura atual
-  if(temp > 23){
-    if(buzz == false){
-      buzz = true;
-      digitalWrite(buzzer, HIGH); //aciona o buzzer
-      int inc_buzz = 0;
-    } 
-    else if(inc_buzz >= 10){
-      digitalWrite(buzzer, LOW);
-      buzz = false;
-    } else {
-      inc_buzz++;
-    }
+  if(temp > 25 && tensao >= 0.4) {
+    digitalWrite(LedRed, HIGH);  //aciona o led
+    digitalWrite(LedYellow, HIGH);
+    digitalWrite(ventoinha, HIGH);
+  }else if(temp > 20){
     digitalWrite(LedRed, HIGH);  //aciona o led
     digitalWrite(ventoinha, HIGH);
-  } else {
-    digitalWrite(buzzer, LOW);
-    digitalWrite(LedRed, LOW);  //aciona o led
-    digitalWrite(ventoinha, LOW);
-  }
-
-// Valida o valor da tensão atual
-  if(tensao <= 0.4 && buzz == false) {
-    if(buzz == false){
-      buzz = true;
-      digitalWrite(buzzer, HIGH); //aciona o buzzer
-      int inc_buzz = 0;
-    } 
-    else if(inc_buzz >= 10){
-      digitalWrite(buzzer, LOW);
-      buzz = false;
-    } else {
-      inc_buzz++;
-    }
+  } else if (tensao >= 4) {
     digitalWrite(LedYellow, HIGH);
+    digitalWrite(ventoinha, HIGH);
   } else {
+    digitalWrite(LedRed, LOW);  //desaciona o led
     digitalWrite(LedYellow, LOW);
-    digitalWrite(buzzer, LOW);
+    digitalWrite(ventoinha, LOW);
   }
   delay(500);
  
+  if(buzz != false && (tensao >= 0.4 || temp > 20)){
+    digitalWrite(buzzer, HIGH); //aciona o buzzer
+    Serial.println("buzzer ligou");  
+  } else {
+    digitalWrite(buzzer, LOW); //desaciona o buzzer
+    Serial.println("buzzer desligou");
+  }
 }
 
 void converte_json()
 {
-  StaticJsonDocument<300> sjson_btn;
+  StaticJsonDocument<300> sjson_temp;
+  StaticJsonDocument<300> sjson_tensao;
+  StaticJsonDocument<300> sjson_vent;
 
-  sjson_btn["variable"] = "temperatura";
-  sjson_btn["value"] = temp;
-  serializeJson(sjson_btn, json_btn);
+  sjson_temp["variable"] = "temperatura";
+  sjson_temp["value"] = temp;
+  serializeJson(sjson_temp, json_temp);
+
+  sjson_tensao["variable"] = "tensao";
+  sjson_tensao["value"] = tensao;
+  serializeJson(sjson_tensao, json_tensao);
+
+  sjson_vent["variable"] = "vent";
+  Serial.println(digitalRead(ventoinha));
+  if(digitalRead(ventoinha) == HIGH){
+    sjson_vent["value"] = "ON";
+  } else {
+    sjson_vent["value"] = "OFF";
+  }
+  serializeJson(sjson_vent, json_vent);
 }
 
 void envia_msg()
 {
-  client.publish("node/btn", json_btn); // You can activate the retain flag by setting the third parameter to true
+  client.publish("node/var", json_temp); // You can activate the retain flag by setting the third parameter to true
+  client.publish("node/var", json_tensao); // You can activate the retain flag by setting the third parameter to true
+  client.publish("node/var", json_vent); // You can activate the retain flag by setting the third parameter to true
 }
 
 void loop() {
@@ -142,34 +148,29 @@ void processa_msg(const String payload)
   if (err) {
     Serial.print(F("deserializeJson() failed with code "));
     Serial.println(err.f_str());
-  }
-  Serial.print("var:");  
+  } 
   String var = msg["variable"];
-  Serial.println(var);
-  if(var == "led")
+  if(var == "buzz")
   {
-    Serial.print("value:");
     String val = msg["value"];
     int var = val.toInt();
-    Serial.println(var);
-    digitalWrite(LedRed, var);
-  }
-  else if(var == "temperatura") {
-    Serial.print("value:");
-    String val = msg["value"];
-    int var = val.toInt();
-    Serial.println(var);
-    analogWrite(temperatura, var);
-  }
- 
+    if(var == 1) {
+      buzz = true;
+      Serial.println("BUZZER ON");  
+    } else {
+      buzz = false;
+      digitalWrite(buzzer, LOW);
+      Serial.println("BUZZER OFF");  
+    }
+  } 
 }
 
 // This function is called once everything is connected (Wifi and MQTT)
 // WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
 void onConnectionEstablished()
 {
-   client.subscribe("node/led", [] (const String &payload)  {
-   Serial.println(payload);
+   client.subscribe("node/envia", [] (const String &payload)  {
+   // Serial.println(payload);
    processa_msg(payload);
   });
 }
